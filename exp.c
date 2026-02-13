d7d58ce7abdf5c1343cfceb9359bf284ab4958f9
Garik Gareginyan <garrogareginyan60@gmail.com>
2026-02-14 01:48:32 +0400

    Vector

--- exp.c
+++ exp.c
@@ -6,72 +6,102 @@
 #define DEFAULT_SIZE 8
 
 typedef struct Vector{
-    TYPE *ptr;
+    TYPE*ptr;
     size_t size;
     size_t capacity;
-} Vector;
+}Vector;
 
 bool constructor(Vector *vector);
 bool reserve(Vector *vector, size_t size);
 void resize(Vector *vector);
 void clear(Vector *vector);
 
+void push_back(Vector *vector, TYPE value);
+void push_front(Vector *vector, TYPE value);
+void pop_back(Vector *vector);
+void pop_front(Vector *vector);
+
+
 int main(){
-    Vector array;
 
-    if(!constructor(&array)){
-        printf("Constructor failed!\n");
-        return 1;
-    }
-
-    printf("%zu\n", array.capacity);
-    printf("%zu\n", array.size);
-
-    clear(&array);
-    return 0;
 }
+
+
+
 
 bool constructor(Vector *vector){
-
-    vector->ptr = malloc(DEFAULT_SIZE * sizeof(TYPE));
-    if(vector->ptr == NULL){
-        perror("Malloc error");
-        return false;
-    }
-
-    vector->capacity = DEFAULT_SIZE;
-    vector->size = 0;
-
-    return true;
+  
+ vector->ptr = malloc (DEFAULT_SIZE * sizeof(TYPE));
+ if(vector->ptr == NULL){
+  perror("Malloc error: ");
+  return false;
 }
+
+ vector->capacity = DEFAULT_SIZE;
+ vector->size = 0;
+  return true;
+
+
+}
+
 
 bool reserve(Vector *vector, size_t size){
+ TYPE *tmp = realloc(vector->ptr, size * sizeof(TYPE));
+if(tmp == NULL){
+    perror("Realloc error");
+    return false;
+}
+vector->ptr = tmp;
 
-    TYPE *temp = realloc(vector->ptr, size * sizeof(TYPE));
-    if(temp == NULL){
-        perror("Realloc error");
-        return false;
-    }
 
-    vector->ptr = temp;
-    vector->capacity = size;
+ vector->capacity = size;
+ return true;
 
-    return true;
+
 }
 
-void resize(Vector *vector){
 
-    if(vector->size == vector->capacity){
-        reserve(vector, vector->capacity * 2);
-    }
+void resize(Vector *vector){
+ if(vector->size == vector->capacity){
+  reserve(vector, vector->capacity * 2);
+}
 
 }
 
 void clear(Vector *vector){
+free(vector->ptr);
+vector->ptr = NULL;
+vector->size = 0;
+vector->capacity = 0;
 
-    free(vector->ptr);
-    vector->ptr = NULL;
-    vector->size = 0;
-    vector->capacity = 0;
+}
 
+void push_back(Vector *vector, TYPE value){
+ resize(vector);
+ vector->ptr[vector->size] = value;
+ ++vector->size;
+}
+
+void pop_back(Vector *vector){
+ --vector->size;
+}
+
+void push_front(Vector *vector, TYPE value){
+ resize(vector);
+ for(int i = vector->size; i > 0; --i){
+    vector->ptr[i] = vector->ptr[i - 1];
+}
+
+
+ vector->ptr[0] = value;
+ ++vector->size;
+}
+
+void pop_front(Vector *vector){
+ for(int i = 0; i < vector->size - 1; ++i){
+  vector->ptr[i] = vector->ptr[i + 1];
+ 
+ }
+
+ --vector->size;
 }
